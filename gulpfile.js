b551fd3391a66f7785f4ce29efd725bedf276704
Dennis Drenner <dennis@dennisdrenner.com>
2015-08-21 14:39:22 -0400

    reorganized project for use with gulp.  app folder for dev code, dist folder for optimized production code

--- gulpfile.js
+++ gulpfile.js
@@ -9,10 +9,46 @@ var uglify = require('gulp-uglify');
 var rename = require('gulp-rename');
 var imagemin = require('gulp-imagemin');
 var pngquant = require('imagemin-pngquant');
+var htmlreplace = require('gulp-html-replace');
+var minifyCss = require('gulp-minify-css');
+
+
+//HTML replace - replace script in development HTML with processed/minified script and move to dist folder for production
+
+gulp.task('htmlReplace', function() {
+  gulp.src('app/*.html')
+    .pipe(htmlreplace({
+        'js': 'js/all.min.js'
+    }))
+    .pipe(gulp.dest('dist'));
+});
+
+gulp.task('htmlReplace2', function() {
+  gulp.src('app/views/*.html')
+    .pipe(htmlreplace({
+       'css': 'css/styles.min.css',
+        'js': 'js/all.min.js'
+    }))
+    .pipe(gulp.dest('dist/views'));
+});
+
+//minify css
+gulp.task('minify-css', function() {
+  return gulp.src('app/css/*.css')
+    .pipe(minifyCss({compatibility: 'ie8'}))
+    .pipe(gulp.dest('dist/css'));
+});
+
+gulp.task('minify-css2', function() {
+  return gulp.src('app/views/css/*.css')
+    .pipe(minifyCss({compatibility: 'ie8'}))
+    .pipe(gulp.dest('dist/views/css'));
+});
+
 
 // Lint Task
 gulp.task('lint', function() {
-    return gulp.src('views/js/*.js')
+    return gulp.src('app/views/js/*.js')
         .pipe(jshint())
         .pipe(jshint.reporter('default'));
 });
@@ -26,23 +62,42 @@ gulp.task('lint', function() {
 
 // Compress images
 gulp.task('imageCompress', function () {
-    return gulp.src('views/images/*')
+    return gulp.src('app/views/images/*')
         .pipe(imagemin({
             progressive: true,
             svgoPlugins: [{removeViewBox: false}],
             use: [pngquant()]
         }))
-        .pipe(gulp.dest('dist/images'));
+        .pipe(gulp.dest('dist/views/images'));
+});
+
+gulp.task('imageCompress2', function () {
+    return gulp.src('app/img/*')
+        .pipe(imagemin({
+            progressive: true,
+            svgoPlugins: [{removeViewBox: false}],
+            use: [pngquant()]
+        }))
+        .pipe(gulp.dest('dist/img'));
 });
 
 // Concatenate & Minify JS
 gulp.task('scripts', function() {
-    return gulp.src('views/js/*.js')
+    return gulp.src('app/views/js/*.js')
         .pipe(concat('all.js'))
-        .pipe(gulp.dest('dist'))
+        .pipe(gulp.dest('dist/views/js'))
         .pipe(rename('all.min.js'))
         .pipe(uglify())
-        .pipe(gulp.dest('dist'));
+        .pipe(gulp.dest('dist/views/js'));
+});
+
+gulp.task('scripts2', function() {
+    return gulp.src('app/js/*.js')
+        .pipe(concat('all.js'))
+        .pipe(gulp.dest('dist/js'))
+        .pipe(rename('all.min.js'))
+        .pipe(uglify())
+        .pipe(gulp.dest('dist/js'));
 });
 
 // Watch Files For Changes
@@ -50,5 +105,6 @@ gulp.task('watch', function() {
     gulp.watch('views/js/*.js', ['lint', 'scripts']);
 });
 
+
 // Default Task
-gulp.task('default', ['lint', 'scripts', 'watch', 'imageCompress']);
+gulp.task('default', ['lint', 'scripts', 'scripts2', 'imageCompress', 'imageCompress2', 'htmlReplace', 'htmlReplace2', 'minify-css', 'minify-css2']);
